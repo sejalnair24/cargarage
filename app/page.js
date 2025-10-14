8872bc465dedcb49392bdbd02c36fc39e52cb869
unknown <sejalnair160@gmail.com>
2025-10-14 17:03:15 +0530

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,107 @@
+export default function Land(){
+    return(
+       <div >
+          <div className="h-[800px] w-full  rounded-[80px] md:mx-150px flex-col bg-black" >
+            
+          <div className=" bg-gray-900  flex flex-row space-x-10 [1300px] rounded-[20px] h-[80px] items-center" >
+        <div className="md:ml-[20px]  w-full">  
+          <img src="https://themehealer.com/php-template/crank-php/assets/images/resources/logo-1.png"/>
+          </div>  
+         <div className="h-[50px] w-[160px]  mx-150px flex flex-row " >
+          <div className=" mt-[13px]  ml-[100px] text-white underline hidden md:block">Home  </div></div>
+          <div className="  hidden md:block text-white underline">AboutUs </div>
+          <div className=" hidden md:block text-white underline">Pages  </div>
+          <div className=" hidden md:block text-white underline">Services </div>
+          <div className=" hidden md:block text-white underline">Shop  </div>
+          <div className="hidden md:block text-white underline">Blog </div>
+          <div className="hidden md:block text-white underline">Contact  </div>
+     
+        <img src="https://themehealer.com/php-template/crank-php/assets/images/resources/main-slider-img-2.jpg"height="500px"width="490px"className="flex flex-row md:mt-[700px] hidden md:block md:mr-[100px]"/>
+    
+          </div>
+          <div className="text-white md:text-6xl mt-[150px] md:ml-[90px] md:ml-[20px] space-x-4">
+           <h1 className=" font-bold  text-6xl"> Where Quality Is A</h1>
+         <h1  className=" font-bold  text-6xl "> Service Meets The</h1> 
+           <h1  className="text-red-500  text-6xl">Open Road</h1>
+             <div className="text-2xl  mt-[80px] box-border size-20 w-[150px] h-[50px] rounded-[30px] border-red-500 border-2 p-2">Get started</div>   
+        
+         <div className="md:h-[200px]   h-[450px] rounded-[40px] md:mx-150px flex flex-col bg-white border-2 border-black md:ml-[200px] mt-[180px] md:w-[900px] w-full py-[80px]" >
+         <div><h1  className="text-black font-bold ml-[50px]">600+</h1><h2 className="flex flex-col text-xl text-black ml-[50px]">Team Member</h2></div> 
+         <div className="absolute"> <h1  className="text-black font-bold  ml-[250px]">2k+</h1><h2 className="flex flex-col text-black text-xl ml-[240px]">Service Complete</h2></div>
+          <div className="absolute"> <h1  className="text-black font-bold  ml-[450px]">53+</h1><h2 className="flex flex-col text-black text-xl ml-[450px]">Winning Award</h2></div>
+          <div className="absolute"> <h1  className="text-black font-bold  ml-[650px]">3k+</h1><h2 className="flex flex-col text-black text-xl ml-[640px]">Client Review</h2></div>
+         </div>
+          </div> 
+         
+          </div>
+          <div className="flex flex-col items-center h-[500px] font-bold">
+          
+          <div className="text-red-500 items-center  mt-[250px]  ">LATEST SERVICE</div>
+         <div className="font-bold text-5xl md:mt-[20px]mt-[30px]">Your Trusted Car Provider<br></br>
+             <h2 className="md:ml-[60px]">Keeping Your Vehicle</h2></div>
+                 <div className="flex flex-row mt-[20px] gap-4 ">
+                   <div className="bg-white border-2 border-black  rounded-[15px] md:h-[300px] h-[500px] md:w-[400px] w-full mt-[10px] hover:bg-gradient-to-r hover:from-red-900 hover:to-blue-900 hover:text-white">
+                    <h2 className="md:mt-[50px] mt-[20px]  ml-[90px] ">Transformation advising</h2>
+                     <h3 className="md:mt-[30px] mt-[20px]  ml-[20px] ">Car service is essential for maintaining the performance and longevity of your vehicle.<br></br> From oil changes Car service</h3>
+                   </div>
+                    <div className="bg-white border-2 border-black  rounded-[15px] md:h-[300px] h-[500px] md:w-[400px] w-full  mt-[10px]  hover:bg-gradient-to-r hover:from-red-900 hover:to-blue-900 hover:text-white">
+                    <h2 className="md:mt-[50px] mt-[20px]  ml-[90px] ">Easy Drive Maintenance</h2>
+                     <h3 className="md:mt-[30px] mt-[20px]  ml-[20px] ">Car service is essential for maintaining the performance and longevity of your vehicle.<br></br> From oil changes Car service</h3>
+                   </div>
+                     <div className="bg-white border-2 border-black  rounded-[15px] md:h-[300px] h-[500px] md:w-[400px] w-full mt-[10px]  hover:bg-gradient-to-r hover:from-red-900 hover:to-blue-900 hover:text-white">
+                    <h2 className="md:mt-[50px] mt-[20px]  ml-[90px] ">Elite Auto Services</h2><br></br>
+                    <h3 className="md:mt-[30px] mt-[20px]  ml-[20px] ">Car service is essential for maintaining the performance and longevity of your vehicle.<br></br> From oil changes Car service</h3>
+                   </div>
+                   </div>
+        
+         
+         
+         
+         
+         
+         
+         
+          </div>
+          <div >
+            <div className="flex md:flex-row flex-col">
+             
+           <div className="text-red-500 md:ml-[250px] h-[500px] mt-[250px] flex flex-col font-bold">ABOUT US
+            <div className="text-black font-bold text-2xl flex flex-col  flex flex-col  "> Fast and Reliable Car Care<br></br>
+                Your Car Our Priority 
+             </div> 
+             <div className="flex flex-col text-black">
+               <h2 className="text-black mt-[10px]">Car service is essential for maintaining the performance and longevity <br></br>of your vehicle. From oil changes</h2>
+          <br></br>  <h3 className="mt-[10px]">Fast and Reliable Car Care Your Car Our Priority</h3>
+           <br></br> <h3>Free with Our Services Care for Your Car </h3>
+            <br></br> <h3>Top-notch Care for Your Vehicle Your Vehicle </h3>
+             <br></br>  <h3>Expert Service for Your Vehicle Drive Stress</h3>
+              <br></br>  <h3>Your Trusted Car Service Provider Keeping</h3>
+             </div>
+               
+             <div className="flex md:flex-row flex-col md:ml-[150px] absolute"> 
+               <img src="https://themehealer.com/php-template/crank-php/assets/images/resources/about-one-img-1.jpg"className="rounded-[50px] md:ml-[500px] md:h-[400px] md:w-[500px] w-full h-[320px] flex md:flex-row flex-col "/>
+                
+                </div>
+             </div>
+           </div> 
+          </div> 
+    
+          <div className=" bg-gray-900 w-full md:ml-[280px] mt-[250px] mb-[10px] md:space-x-10 md:w-[900px] w-full rounded-[20px] md:h-[120px] h-[650px] items-center" >
+            
+            <div className="flex md:flex-row flex-col w-full mt-[90px]">
+              <img src="https://themehealer.com/php-template/crank-php/assets/images/brand/brand-1-5.png "className="md:mt-[5px] mt-[10px] ml-[18px]  py-[25px] "/>
+           <img src="https://themehealer.com/php-template/crank-php/assets/images/brand/brand-1-4.png"className="md:mt-[5px] mt-[5px] ml-[10px] py-[25px]"/>
+              <img src="https://themehealer.com/php-template/crank-php/assets/images/brand/brand-1-1.png"className="md:mt-[5px]  mt-[5px] py-[25px] ml-[10px]"/>
+              <img src="https://themehealer.com/php-template/crank-php/assets/images/brand/brand-1-3.png"className="md:mt-[5px] mt-[5px] py-[25px] ml-[10px]"/>
+              <img src="https://themehealer.com/php-template/crank-php/assets/images/brand/brand-1-2.png"className="md:mt-[5px]  mt-[5px]  py-[25px] ml-[10px]"/>
+           </div>
+            </div>   
+           </div>
+    
+    )
+  }
+        
+  
+
+       
+        
---
+++ landing.js
@@ -0,0 +1,11 @@
+
+export default function Home(){
+  return(<div style={{color:"black",display:"flex",flexDirection:"horizontal"}} >
+    <img src="https://static.vecteezy.com/system/resources/thumbnails/005/544/708/small_2x/profile-icon-design-free-vector.jpg"height="100px"width="100px"/>
+  
+   <h2 style={{marginTop:"20px",padding:"10px",backgroundColor:"pink",borderRadius:"10px",width:"150px",height:"50px"}}>Sejal Nair</h2>
+  <div style={{display:"flex",flexDirection:"vertical"}}>
+   <img src="https://images.pexels.com/photos/863963/pexels-photo-863963.jpeg?cs=srgb&dl=nature-flowers-garden-863963.jpg&fm=jpg "height="250px"width="250px"/>
+  </div>
+  </div>);
+}
