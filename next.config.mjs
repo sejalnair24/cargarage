c054332f1107f23e3842fef094fa7e9d76efe8ce
unknown <sejalnair160@gmail.com>
2025-11-21 21:39:12 +0530

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-module.exports = {
+const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
+
 export default nextConfig;
